4198eacd2f55335d2648277602454905c3396cf1
Sean <sweickel@gmail.com>
2020-08-08 01:25:14 -0400

    redesigned website and added projects

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+function init() {
+  const dom = document.querySelector.bind(document);
+
+  dom("#port-arrow").addEventListener("click", (event) => {
+    if (dom("#port-tiles").className === "wrap-closed") {
+      dom("#port-tiles").classList.remove("wrap-closed");
+      dom("#port-tiles").classList.add("tile-wrapper");
+      dom("#port-arrow").src = "/images/up.png";
+      dom("#portfolio").style.flexDirection = "column";
+    } else if (dom("#port-tiles").className === "tile-wrapper") {
+      dom("#port-tiles").classList.remove("tile-wrapper");
+      dom("#port-tiles").classList.add("wrap-closed");
+      dom("#port-arrow").src = "/images/down.png";
+      dom("#portfolio").style.flexDirection = "row";
+    }
+  });
+}
+
+window.addEventListener("load", (event) => {
+  init();
+});
